d56c9668a448c3b10648fe15b79d24acec660908
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2024-11-18 17:43:57 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello")
+console.log(3+3)
+
+console.log("Somnath")
+
+let name = "Somnath"
+let age = 23
+let isLoggedIn = false
+
+console.log(typeof undefined); //Undefined
+console.log(typeof null); //Object
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "somnath@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword = "121212"
+accountCity = "Dhanbad"
+
+//accountId = 2 //not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
