ceddca17edf73c524b56389883fd874999181d53
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2024-11-19 17:55:01 +0000

    Datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+//Primitive datatypes
+
+// 7 types : String, number, boolean, null, undefined, Symbol, BigInt 
+
+const score  = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+// Reference( Non - Primitive )
+                                                                                         
+//Arrays, Objects, functions
+
+const heros = ["tom", "jerry", 'ben'];
+
+let myObj = {
+    name: 'hitesh',
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
